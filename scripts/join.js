9e78c4c969c3cee175210128e5c146fbe7ddf174
Alexis Leroy <a.leroy@mentalworks.fr>
2018-07-11 17:16:34 +0200

    trying out dialogflow

--- scripts/join.js
+++ scripts/join.js
@@ -9,6 +9,43 @@ module.exports.run = member => {
                 const audio = receiver.createPCMStream(user);
                 const output = generateOutputFile(member.voiceChannel, member.user);
                 audio.pipe(output);
+//                 audio.on('end',() => {
+//                     // Imports the Dialogflow library
+//                     const dialogflow = require('dialogflow');
+//                     const common = require('@google-cloud/common')
+// // Instantiates a sessison client
+//                     const sessionClient = new dialogflow.SessionsClient();
+//
+// // The path to identify the agent that owns the created intent.
+//                     const sessionPath = sessionClient.sessionPath('no-swearing-bot', 'test');
+//
+// // Read the content of the audio file and send it as part of the request.
+//                     const readFile = common.util.promisify(fs.readFile, {singular: true});
+//                     readFile(output.path)
+//                         .then(inputAudio => {
+//                             // The audio query request
+//                             const request = {
+//                                 session: sessionPath,
+//                                 queryInput: {
+//                                     audioConfig: {
+//                                         audioEncoding: 'pcm',
+//                                         sampleRateHertz: sampleRateHertz,
+//                                         languageCode: 'fr-FR',
+//                                     },
+//                                 },
+//                                 inputAudio: inputAudio,
+//                             };
+//                             // Recognizes the speech in the audio and detects its intent.
+//                             return sessionClient.detectIntent(request);
+//                         })
+//                         .then(responses => {
+//                             console.log('Detected intent:');
+//                             logQueryResult(sessionClient, responses[0].queryResult);
+//                         })
+//                         .catch(err => {
+//                             console.error('ERROR:', err);
+//                         });
+//                 });
             }
         });
     });
@@ -16,5 +53,6 @@ module.exports.run = member => {
 
 function generateOutputFile(channel, member) {
     const fileName = `./recordings/${channel.id}-${member.id}-${Date.now()}.pcm`;
+   fs.createWriteStream(fileName);
     return fs.createWriteStream(fileName);
 }
